75f10a25e631683762c51683b4832606fa06c0a0
Kaushik Ravikumar <kaushik.ravikumar1999@gmail.com>
2018-07-10 15:19:22 -0700

    clean up Admin JS code

--- admin_app/index.js
+++ admin_app/index.js
@@ -1,5 +1,34 @@
 var pubnub;
 
+const url = 'https://pubsub.pubnub.com/v1/blocks/sub-key/sub-c-3436c79c-78a1-11e8-a43f-d6f8762e29f7/getanswers';
+
+const firstjsonReqOptions = {
+    "body": {
+        "which": "a"
+    }
+};
+
+const secondjsonReqOptions = {
+    "body": {
+        "which": "b"
+    }
+};
+
+const thirdjsonReqOptions = {
+    "body": {
+        "which": "c"
+    }
+};
+
+const fourthjsonReqOptions = {
+    "body": {
+        "which": "d"
+    }
+};
+
+/*
+*  This function is called when the Admin user presses the submit button.
+*/
 function publishQuestionAnswer() {
     pubnub = new PubNub({
         subscribeKey: "sub-c-3436c79c-78a1-11e8-a43f-d6f8762e29f7",
@@ -8,47 +37,37 @@ function publishQuestionAnswer() {
         ssl: true
     });
 
-      pubnub.publish({
-              message: {
-                  "question": document.getElementById('question').value,
-                  "optionA": document.getElementById('optionA').value,
-                  "optionB": document.getElementById('optionB').value,
-                  "optionC": document.getElementById('optionC').value,
-                  "optionD": document.getElementById('optionD').value
-              },
-              channel: 'question_post',
-              sendByPost: false, // true to send via post
-              storeInHistory: false //override default storage options
-          },
-          function(status, response) {
-              if (status.error) {
-                  // handle error
-                  console.log(status);
-              } else {
-                  console.log("message Published w/ timetoken", response.timetoken);
-              }
-          });
+    pubnub.publish({
+            message: {
+                "question": document.getElementById('question').value,
+                "optionA": document.getElementById('optionA').value,
+                "optionB": document.getElementById('optionB').value,
+                "optionC": document.getElementById('optionC').value,
+                "optionD": document.getElementById('optionD').value
+            },
+            channel: 'question_post',
+            sendByPost: false, // true to send via post
+            storeInHistory: false //override default storage options
+        },
+        function(status, response) {
+            if (status.error) {
+                // handle error
+                console.log(status);
+            } else {
+                console.log("message Published w/ timetoken", response.timetoken);
+            }
+        });
 
-    // WAIT FOR 12 SECONDS AND THEN PUBLISH ANSWER!!
+    // Waits for 12 seconds then publishes correct answer and answer results.
     // Want to consider up to 2 second latency between answers being sent in and results being published.
     setTimeout(getResults, 12000);
 }
 
-var url = 'https://pubsub.pubnub.com/v1/blocks/sub-key/sub-c-3436c79c-78a1-11e8-a43f-d6f8762e29f7/getanswers';
-
-var firstjsonReq = {
-    "which": "a"
-};
-var secondjsonReq = {
-    "which": "b"
-};
-var thirdjsonReq = {
-    "which": "c"
-};
-var fourthjsonReq = {
-    "which": "d"
-};
-
+/*
+* This function gets the correct answer from the admin's entry. It then makes chained promisified XMLHttpRequests
+* to the getAnswers PubNub function with the route specified as 'getcount' in order to obtain the count of how many people
+* answered each option. Then publishes this data and resets the counters.
+*/
 function getResults() {
     var correctAnswer;
     if (document.getElementById('a_correct').checked) {
@@ -61,56 +80,34 @@ function getResults() {
         correctAnswer = "optionD";
     }
 
-    var firstRequest = new XMLHttpRequest();
-    var secondRequest = new XMLHttpRequest();
-    var thirdRequest = new XMLHttpRequest();
-    var fourthRequest = new XMLHttpRequest();
-
-    firstRequest.open('POST', url + '?route=getcount');
-    firstRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    secondRequest.open('POST', url + '?route=getcount');
-    secondRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    thirdRequest.open('POST', url + '?route=getcount');
-    thirdRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    fourthRequest.open('POST', url + '?route=getcount');
-    fourthRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    firstRequest.onreadystatechange = function() {
-        if (firstRequest.readyState == XMLHttpRequest.DONE) {
-            var countA = JSON.parse(firstRequest.responseText).optionA;
-
-            secondRequest.onreadystatechange = function() {
-                if (secondRequest.readyState == XMLHttpRequest.DONE) {
-                    var countB = JSON.parse(secondRequest.responseText).optionB;
-
-                    thirdRequest.onreadystatechange = function() {
-                        if (thirdRequest.readyState == XMLHttpRequest.DONE) {
-                            var countC = JSON.parse(thirdRequest.responseText).optionC;
-
-                            fourthRequest.onreadystatechange = function() {
-                                if (fourthRequest.readyState == XMLHttpRequest.DONE) {
-                                    var countD = JSON.parse(fourthRequest.responseText).optionD;
-                                    publishMessage(countA, countB, countC, countD, correctAnswer);
-                                }
-                            };
-                            fourthRequest.send(JSON.stringify(fourthjsonReq));
-                        }
-                    };
-                    thirdRequest.send(JSON.stringify(thirdjsonReq));
-                }
-            };
-            secondRequest.send(JSON.stringify(secondjsonReq));
-        }
-    };
-    firstRequest.send(JSON.stringify(firstjsonReq));
-
-    // // Waits a second then reset counters
-    setTimeout(resetCounters, 1000);
+    return request(url + '?route=getcount', 'POST', firstjsonReqOptions).then((firstResponse) => {
+        var countA = firstResponse.optionA;
+        return request(url + '?route=getcount', 'POST', secondjsonReqOptions).then((secondResponse) => {
+            var countB = secondResponse.optionB;
+            return request(url + '?route=getcount', 'POST', thirdjsonReqOptions).then((thirdResponse) => {
+                var countC = thirdResponse.optionC;
+                return request(url + '?route=getcount', 'POST', fourthjsonReqOptions).then((fourthResponse) => {
+                    var countD = fourthResponse.optionD;
+                    publishMessage(countA, countB, countC, countD, correctAnswer);
+                    // Waits a second then reset counters
+                    setTimeout(resetCounters, 1000);
+                })
+            })
+        })
+    }).catch((error) => {
+        console.log(error);
+    });
 }
 
+/**
+* This function is called by getResults() to publish the answer results onto the answer_post channel for users
+* to see the correct answer and the stats of how many people answered which option.
+* @param {Integer} countA number of users that answered optionA
+* @param {Integer} countB number of users that answered optionB
+* @param {Integer} countC number of users that answered optionC
+* @param {Integer} countD number of users that answered optionD
+* @param {String} correctAnswer either 'optionA', 'optionB', 'optionC', or 'optionD'
+*/
 function publishMessage(countA, countB, countC, countD, correctAnswer) {
     pubnub.publish({
             message: {
@@ -134,47 +131,69 @@ function publishMessage(countA, countB, countC, countD, correctAnswer) {
         });
 }
 
-function resetCounters()
-{
-  var firstRequest = new XMLHttpRequest();
-  var secondRequest = new XMLHttpRequest();
-  var thirdRequest = new XMLHttpRequest();
-  var fourthRequest = new XMLHttpRequest();
+/*
+* This function is called by getResults() to reset all our counters in the KV Store.
+* This is done by calling our getAnswers PubNub function, which is modeled as a Rest API.
+* We will specify the route as 'reset', so the appropriate function is executed.
+*/
+function resetCounters() {
+    return request(url + '?route=reset', 'POST', firstjsonReqOptions).then((firstResponse) => {
+        return request(url + '?route=reset', 'POST', secondjsonReqOptions).then((secondResponse) => {
+            return request(url + '?route=reset', 'POST', thirdjsonReqOptions).then((thirdResponse) => {
+                return request(url + '?route=reset', 'POST', fourthjsonReqOptions).then((fourthResponse) => {
+                    console.log('Reset all counters!');
+                })
+            })
+        })
+    }).catch((error) => {
+        console.log(error);
+    });
+}
 
-  firstRequest.open('POST', url + '?route=reset');
-  firstRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-  secondRequest.open('POST', url + '?route=reset');
-  secondRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-  thirdRequest.open('POST', url + '?route=reset');
-  thirdRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-  fourthRequest.open('POST', url + '?route=reset');
-  fourthRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-  firstRequest.onreadystatechange = function() {
-      if (firstRequest.readyState == XMLHttpRequest.DONE) {
-
-          secondRequest.onreadystatechange = function() {
-              if (secondRequest.readyState == XMLHttpRequest.DONE) {
-
-                thirdRequest.onreadystatechange = function() {
-                    if (thirdRequest.readyState == XMLHttpRequest.DONE) {
-
-                      fourthRequest.onreadystatechange = function() {
-                          if (fourthRequest.readyState == XMLHttpRequest.DONE) {
-                            // RESETS COUNTERS
-                          }
-                      };
-                      fourthRequest.send(JSON.stringify(fourthjsonReq));
-                    }
-                };
-                thirdRequest.send(JSON.stringify(thirdjsonReq));
-              }
-          };
-          secondRequest.send(JSON.stringify(secondjsonReq));
-      }
-  };
-  firstRequest.send(JSON.stringify(firstjsonReq));
+/**
+ * Helper function to make an HTTP request wrapped in an ES6 Promise.
+ *
+ * @param {String} url URL of the resource that is being requested.
+ * @param {String} method POST, GET, PUT, etc.
+ * @param {Object} options JSON Object with HTTP request options, "header"
+ *     Object of possible headers to set, and a body Object of a request body.
+ *
+ * @return {Promise} Resolves a parsed JSON Object or String response text if
+ *     the response code is in the 200 range. Rejects with response status text
+ *     when the response code is outside of the 200 range.
+ */
+function request(url, method, options) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        let contentTypeIsSet = false;
+        options = options || {};
+        xhr.open(method, url);
+        for (let header in options.headers) {
+            if ({}.hasOwnProperty.call(options.headers, header)) {
+                header = header.toLowerCase();
+                contentTypeIsSet = header === 'content-type' ? true : contentTypeIsSet;
+                xhr.setRequestHeader(header, options.headers[header]);
+            }
+        }
+        if (!contentTypeIsSet) {
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        }
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                let response;
+                try {
+                    response = JSON.parse(xhr.response);
+                } catch (e) {
+                    response = xhr.response;
+                }
+                resolve(response);
+            } else {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText,
+                });
+            }
+        };
+        xhr.send(JSON.stringify(options.body));
+    });
 }
