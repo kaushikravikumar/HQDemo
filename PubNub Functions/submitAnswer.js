78a3318e7952d3e5a12739801a51764ba1873cc1
kaushikravikumar <kaushik.ravikumar1999@gmail.com>
2018-07-16 13:12:58 -0700

    Update submitAnswer.js

--- PubNub Functions/submitAnswer.js
+++ PubNub Functions/submitAnswer.js
@@ -4,25 +4,12 @@ export default (request) => {
 
     var answer = request.message.nameValuePairs.answer;
 
-    if(answer === "optionA")
-    {
-        kvstore.incrCounter("optionA", 1);
-        console.log('A Incremented');
-    }
-    else if(answer === "optionB")
-    {
-        kvstore.incrCounter("optionB", 1);
-        console.log('B Incremented');
-    }
-    else if(answer === "optionC")
-    {
-        kvstore.incrCounter("optionC", 1);
-        console.log('C Incremented');
-    }
-    else if(answer === "optionD")
-    {
-        kvstore.incrCounter("optionD", 1);
-        console.log('D Incremented');
+    if (answer !== "optionA" || answer !== "optionB" || answer !== "optionC" ||
+        answer !== "optionD") {
+        response.status = 400;
+        return response.send();
+    } else {
+        kvstore.incrCounter(answer, 1);
     }
     return request.ok(); // Return a promise when you're done
 }
